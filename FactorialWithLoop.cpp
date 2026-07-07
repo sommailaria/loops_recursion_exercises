9d92553ec3be9de1ad1be781798f9e734e911676
Ilaria <mail@ilariasomma.io>
2026-07-07 20:53:56 +0200

    Created FactorialWithLoop.cpp

---
+++ FactorialWithLoop.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+// Given a large number N, task is to find the factorial of N.
+// Example: factorial of 6 is 6*5*4*3*2*1 = 720.
+std::vector<int> multiplyNum(long int num, std::vector<int> digits)
+{
+    long int carryDigits = 0;
+
+    for (std::vector<int>::size_type i = 0; i < digits.size(); ++i)
+    {
+        long int storeCalculation = digits[i] * num + carryDigits;
+        digits[i] = storeCalculation % 10;
+        carryDigits = storeCalculation / 10;
+    }
+
+    while (carryDigits)
+    {
+        digits.push_back(carryDigits % 10);
+        carryDigits = carryDigits / 10;
+    }
+
+    return digits;
+}
+
+// Function with loop
+std::vector<int> calculateFactorial(long int baseNum)
+{
+    std::vector<int> myDigits = {1};
+
+    for (long int i = 2; i <= baseNum; ++i)
+    {
+        myDigits = multiplyNum(i, myDigits);
+    }
+
+    return myDigits;
+}
+
+int main()
+{
+    long int bigNum = 100;
+
+    std::vector<int> calcResult = calculateFactorial(bigNum);
+
+    for (std::vector<int>::size_type i = calcResult.size(); i > 0; --i)
+    {
+        std::cout << calcResult[i - 1];
+    }
+
+    return 0;
+}
